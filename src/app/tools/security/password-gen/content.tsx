5cc9e41c2505f8f2565b1ce16c5df8b930a959e3
dragusheen <nathan.tirolf@epitech.eu>
2026-02-18 09:57:31 -0500

    [+] add password generation tool layout

---
+++ src/app/tools/security/password-gen/content.tsx
@@ -0,0 +1,26 @@
+/*
+	Authors:
+	>> Nathan TIROLF - { nathan.tirolf@epitech.eu }
+
+	(„• ֊ •„)❤  <  Have a good day !
+	--U-----U------------------------
+*/
+
+"use client";
+
+/* ----- IMPORTS ----- */
+import { useState } from "react";
+
+
+/* ----- COMPONENT ----- */
+function WordCounterContent() {
+	return (
+		<div className="text-lg">
+			Générateur de mot de passe en préparation...
+		</div>
+	);
+}
+
+
+/* ----- EXPORT ----- */
+export default WordCounterContent;
